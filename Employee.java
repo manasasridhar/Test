b72c5f4a4343aced09fc52b5450c80c07c50224b
Sridhar <srinidhi123@gmail.com>
2018-11-28 22:00:11 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,26 @@
+class Employee{
+
+string firstname;
+string lastname;
+int id;
+
+public String getFirstname(){
+	
+	return firstname;
+}
+
+public String getLastname(){
+	
+	return lastname;
+}
+
+
+public int getId(){
+	
+	return id;
+}
+
+
+
+
+}
